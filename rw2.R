87dd5eee0bb9fdc30b87d1a15459b4b6ab2abf62
Katie Paulson <krpaulson@gmail.com>
2023-11-03 15:11:03 -0700

    add inla for comparison

--- rw2.R
+++ rw2.R
@@ -3,6 +3,7 @@
 
 library(tidyverse)
 library(TMB)
+library(INLA)
 
 # example data
 # log-logistic survival model fit, for illustration smooth log-shape parameter only
@@ -10,6 +11,9 @@ load("2014_loglogistic.RData")
 df <- fit_loglogistic$result
 V <- fit_loglogistic$variance[1:20, 1:20]
 n_years <- 20
+
+
+# TMB ---------------------------------------------------------------------
 
 # RW2 structure matrix
 inla.rw = utils::getFromNamespace("inla.rw", "INLA")
@@ -44,5 +48,36 @@ ggplot(data = df, aes(x = period)) +
   geom_line(aes(y = log_shape_smoothed)) +
   geom_ribbon(aes(ymin = log_shape_smoothed - 1.96*sqrt(log_shape_smoothed_var),
                   ymax = log_shape_smoothed + 1.96*sqrt(log_shape_smoothed_var)),
+              alpha = 0.2) +
+  theme_bw()
+
+
+# inla --------------------------------------------------------------------
+
+# for comparison fit a RW2 model in INLA
+df$period_copy <- df$period
+formula <- log_shape_mean ~ 
+  f(period, model = "rw2", constr = T, scale.model = T) +
+  f(period_copy, model = "iid")
+fit_inla <- inla(
+  formula = formula,
+  data = df,
+  family = "gaussian",
+  control.family = list(hyper = list(prec = list(initial = log(1), fixed = TRUE))),
+  scale = 1 / df$log_shape_var, # fixed known variance (note: only uses diagonals, not whole V matrix)
+  control.predictor = list(compute = TRUE),
+  control.compute = list(return.marginals = TRUE, config = TRUE,
+                         return.marginals.predictor = TRUE)
+)
+
+# plot
+df$log_shape_smoothed_inla <- fit_inla$summary.fitted.values$`0.5quant`
+df$log_shape_smoothed_lower_inla <- fit_inla$summary.fitted.values$`0.025quant`
+df$log_shape_smoothed_upper_inla <- fit_inla$summary.fitted.values$`0.975quant`
+ggplot(data = df, aes(x = period)) +
+  geom_point(aes(y = log_shape_mean)) +
+  geom_line(aes(y = log_shape_smoothed_inla)) +
+  geom_ribbon(aes(ymin = log_shape_smoothed_lower_inla,
+                  ymax = log_shape_smoothed_upper_inla),
               alpha = 0.2) +
   theme_bw()
